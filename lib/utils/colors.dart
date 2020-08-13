cff4b47c1f2c7d7540fe9c6db51d08b3aef288ed
0zzycodes <horlalekan123@gmail.com>
2020-08-13 06:53:27 +0100

    Imported the colors to utils folder and set the app theme, also add landing.dart to screen folder and imported it to main.dart

---
+++ lib/utils/colors.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static final Color primaryColor = Color(0XFFffffff);
+  static final Color secondaryColor = Color(0XFF483F8E);
+  static final Color textInputBgColor = Color(0XFFF2F2F2);
+  static final Color borderColor = Color(0XFF716D88);
+  static final Color facebookColor = Color(0XFF9BABCC);
+  static final Color facebookButtonColor = Color(0XFF385798);
+  static final Color twitterColor = Color(0XFF8CD3F1);
+  static final Color twitterButtonColor = Color(0XFF1BA7E2);
+  static final Color googleColor = Color(0XFFF2A698);
+  static final Color googleButtonColor = Color(0XFFE34F31);
+}
